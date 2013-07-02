3f467515a76cd11836c484e8222931136e650e6d
Daniel Davidson <dan@bitmono.com>
2013-07-02 02:06:47 +0100

    Seperate zprofile file for path, updated install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gvimrc oh-my-zsh vim vimrc zshrc"    # list of files/folders to symlink in homedir
+files="oh-my-zsh vim gvimrc vimrc zprofile zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
---
+++ zprofile
@@ -0,0 +1,6 @@
+# Customize to your needs...
+
+export EDITOR=vim
+export PATH="/usr/local/bin:$PATH:/usr/bin:/usr/sbin"
+
+eval "$(rbenv init -)"
